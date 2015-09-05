6515dec0e75fc0032a0149e8bbf8103689500689
Baruch Even <baruch@ev-en.org>
2015-09-05 23:25:12 +0300

    Make sniffer data buffer unsigned char
    This allows printing it as unsigned hex

--- sniffer_data.h
+++ sniffer_data.h
@@ -6,7 +6,7 @@ struct sniffer_data {
 	unsigned long queue_time_usec;
 	unsigned id;
 	uint8_t type; // 0=Request, 1=Response (future would be abort task, target reset)
-	char data[16]; // CDB or Sense
+	unsigned char data[16]; // CDB or Sense
 };
 
 #define SNIFFER_DATA_TYPE_SUBMIT 0
